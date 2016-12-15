90fb73549aa79232dd3d781da51a061e122d5b9b
Edvard <EdvardLoktev@gmail.com>
2016-12-15 22:07:39 +0300

    add CSV  output support

--- src/main/java/com/manahealth/json_comparator/Comparator.java
+++ src/main/java/com/manahealth/json_comparator/Comparator.java
@@ -1,28 +1,52 @@
 package com.manahealth.json_comparator;
 
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flipkart.zjsonpatch.JsonDiff;
+import com.github.opendevl.JFlat;
 
 public class Comparator {
 
+	public enum OutputType {
+		JSON, CSV
+	}
+
+	/***
+	 * 
+	 * @param args:  args[0] - path of beforeJson, args[1] - after, args[2] - diff file destination, args[3] - output type JSON or CSV
+	 * @throws JsonProcessingException
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws JsonProcessingException, IOException {
 
 		String beforeFilePath = args[0];
 		String afterFilePath = args[1];
 		String diffPath = args[2];
+		OutputType outputType = OutputType.valueOf(args[3].trim());
 
 		Comparator comparator = new Comparator();
 
 		JsonNode diff = comparator.getComparedJsons(beforeFilePath, afterFilePath, diffPath);
-		comparator.writeStringToFile(diffPath, diff.toString());
+
+		switch (outputType) {
+		case JSON:
+			comparator.writeStringToFile(diffPath, diff.toString());
+			break;
+		case CSV:
+			jsonToCsv(diffPath, diff.toString());
+			break;
+		}
+
 	}
 
 	private JsonNode getComparedJsons(String beforeFilePath, String afterFilePath, String diffPath) {
@@ -51,4 +75,10 @@ public class Comparator {
 			e.printStackTrace();
 		}
 	}
+
+	private static void jsonToCsv(String filePath, String content) throws FileNotFoundException, UnsupportedEncodingException {
+		JFlat flatMe = new JFlat(content);
+		flatMe.write2csv("filePath");
+	}
+
 }
